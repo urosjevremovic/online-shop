916ef7432286a4a1cfa9b9507b1d265d07b72d59
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-21 23:07:41 +0200

    added a cart app and inside it a Cart class for managing shopping cart, that will be saved in session

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from shop.models import Category, Product
