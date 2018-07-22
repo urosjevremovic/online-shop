7d46674e1c03c6ef1322f90388d89e1f7d09fd8f
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-22 12:58:43 +0200

    added views for viewing cart detail, updating and deleting cart. Added some css, needs working on

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import UpdateView, FormView, DeleteView, DetailView
+from django.views.generic.base import View
 
-# Create your views here.
+from cart.cart import Cart
+from cart.forms import CartAddProductForm
+from shop.models import Product
+
+
+class UpdateCart(FormView):
+    http_method_names = ['post', ]
+    form_class = CartAddProductForm
+    success_url = 'cart:cart_detail'
+
+    def __init__(self):
+        super().__init__()
+        self.cart = Cart(self.request)
+
+    def form_valid(self, form):
+        cd = form.cleaned_data
+        self.cart.add(get_object_or_404(Product, id=self.kwargs['product_id']))
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+class CartDetail(DetailView):
+    model = Cart
+    template_name = 'cart/detail.html'
+    context_object_name = 'cart'
--- shop/urls.py
+++ shop/urls.py
@@ -7,6 +7,5 @@ app_name = 'shop'
 urlpatterns = [
     path('<slug:category_slug>/', ProductList.as_view(), name='product_list_by_category'),
     path('', ProductList.as_view(), name='product_list'),
-
     path('<int:id>/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
     ]
