d3bb2d0d0e5d3c8aba88286de6584a0214f5782e
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-23 17:28:50 +0200

    added order view to admin panel

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from orders.views import order_create
+from orders.views import order_create, AdminOrderDetail, admin_order_detail
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', AdminOrderDetail.as_view(), name='admin_order_detail'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import DetailView
 
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
-from orders.models import OrderItem
+from orders.models import OrderItem, Order
 from orders.tasks import order_created
 
 
@@ -18,10 +21,21 @@ def order_create(request):
                                          quantity=item['quantity'])
             cart.clear()
             order_created.delay(order.id)
-            # set the order in the session
             request.session['order_id'] = order.id
-            # redirect for payment
             return redirect(reverse('payment:process'))
     else:
         form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+
+
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class AdminOrderDetail(DetailView):
+    model = Order
+    template_name = 'admin/orders/order/detail.html'
+    context_object_name = 'order'
+    pk_url_kwarg = 'order_id'
