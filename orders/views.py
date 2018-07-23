3d2c24d6569d8e72ea172b5a71834e8c4f5a8d04
urosjevremovic <jevremovic.uros91@gmail.com>
2018-07-23 11:44:46 +0200

    added celery with Rabbitmq for asynchronous ordering

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from orders.tasks import order_created
 
 
 def order_create(request):
@@ -15,6 +16,7 @@ def order_create(request):
                 OrderItem.objects.create(order=order, product=item['product'], price=item['price'],
                                          quantity=item['quantity'])
             cart.clear()
+            order_created.delay(order.id)
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
